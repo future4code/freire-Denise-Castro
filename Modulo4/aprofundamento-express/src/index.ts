63445a8eeab49dd2befd919ad6bbce8347b4915d
Denise Machado Castro <denise.castro1993ii@gmail.com>
2022-08-17 05:24:58 -0300

    exercicio 01 concluído

--- Modulo4/aprofundamento-express/src/index.ts
+++ Modulo4/aprofundamento-express/src/index.ts
@@ -1,8 +1,20 @@
-import express from "express";
-
+import express, {request, Request, Response} from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { AddressInfo } from "net";
 
-const app = express();
+const app = express(); 
+app.use(cors()); 
+
+/*Exercicio 1
+Faça novamente a instalação e configuração do Express na pasta do exercício. Para testar, crie um endpoint que aponte para o path “/ping”. Esse endpoint pode responder apenas com uma mensagem “pong”.*/
+
+app.get("/ping", (req:Request, res:Response) => {
+    res.send("Pong")
+})
+
+
+
 
 app.use(express.json());
 
