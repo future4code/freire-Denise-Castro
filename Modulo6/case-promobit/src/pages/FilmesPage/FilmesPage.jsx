a74431223809a9852d90de6c5afa5ef95afe917b
Denise Machado Castro <denise.castro1993ii@gmail.com>
2022-10-12 01:38:23 -0300

    Rotas funcionando

---
+++ Modulo6/case-promobit/src/pages/FilmesPage/FilmesPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FilmesPage = () => {
+    return (
+        <h1>Filmes Page</h1>
+    )
+}
+
+export default FilmesPage
---
+++ Modulo6/case-promobit/src/routes/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import HomePage from "../pages/HomePage/HomePage";
+import FilmesPage from "../pages/FilmesPage/FilmesPage";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<HomePage/>}/>
+                <Route path="filmes/:id" element={<FilmesPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
